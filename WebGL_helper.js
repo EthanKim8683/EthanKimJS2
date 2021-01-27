dc36171e120ecd3e7befcb36ea72c22b84c655c3
Ethan Kim <75972308+EthanKim8683@users.noreply.github.com>
2021-01-26 16:17:29 -0800

    Update WebGL_helper.js

--- WebGL_helper.js
+++ WebGL_helper.js
@@ -1,14 +1,16 @@
 var canvas = document.getElementById("canvas")
 var gl = canvas.getContext("webgl2", {preserveDrawingBuffer: true, antialias:false})
+
+var EKGL = function() {
     
-var createShader = function(type,code) {
+this.createShader = function(type,code) {
 	var shader = gl.createShader(type);
 	gl.shaderSource(shader,code);
 	gl.compileShader(shader);
 	return shader;
 };
     
-var createProgram = function(vertexCode,fragmentCode) {
+this.createProgram = function(vertexCode,fragmentCode) {
 	var vertexShader = createShader(gl.VERTEX_SHADER,vertexCode);
 	var fragmentShader = createShader(gl.FRAGMENT_SHADER,fragmentCode);
 	var program = gl.createProgram();
@@ -21,3 +23,18 @@ var createProgram = function(vertexCode,fragmentCode) {
 	gl.deleteShader(fragmentShader);
 	return program;
 }
+
+this.rotate3D = function(rx,ry) {
+    var 
+    cx = Math.cos(ry),
+    sx = Math.sin(ry),
+    cy = Math.cos(rx),
+    sy = Math.sin(rx);
+    return [
+        cy,-sy*sx,-sy*cx,
+        0,cx,-sx,
+        sy,sx*cy,cx*cy,
+    ];
+};
+	
+};
